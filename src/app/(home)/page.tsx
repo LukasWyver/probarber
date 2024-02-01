15cb51b7199a04cf2479fcebaae50f11b4387036
lukaswyver <lucas_sin5@hotmail.com>
2024-01-31 21:01:26 -0300

    feat: add greetings message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,23 @@
-import Header from "@/_components/header";
 import Image from "next/image";
+import { format } from 'date-fns'
+import { ptBR } from "date-fns/locale/pt-BR";
+
+
+import Header from "@/_components/header";
 
 export default function Home() {
   return (
     <div>      
       <Header />
+
+      <div id="greetings" className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Lucas!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM",{
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
