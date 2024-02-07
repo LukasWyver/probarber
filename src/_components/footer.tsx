f8fc9b1c9b2e11c7151b52ea216e0b2f60f1b712
lukaswyver <lucas_sin5@hotmail.com>
2024-02-06 23:38:13 -0300

    feat: ensure semantic tags are used on footer and header

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -2,10 +2,12 @@ import { Card, CardContent } from "./ui/card";
 
 export default function Footer() {
   return (
-    <Card className="rounded-b-none mt-[4.5rem]">
-      <CardContent className="p-5 flex items-center justify-center">
-        <p className="text-xs text-muted-foreground opacity-75">&copy; 2024 • Copyright <strong>PRO Barber</strong></p>  	
-      </CardContent>
-    </Card>
+    <footer className="mt-[4.5rem]">
+      <Card className="rounded-b-none">
+          <CardContent className="p-5 flex items-center justify-center">
+            <p className="text-xs text-muted-foreground opacity-75">&copy; 2024 • Copyright <strong>PRO Barber</strong></p>  	
+          </CardContent>
+      </Card>
+    </footer>
   );
 }
--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -10,24 +10,26 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <Card className="rounded-t-none">
-      <CardContent className="p-5 flex items-center justify-between">
-        <Link href="/">
-          <Image src="/ProBarber.svg" alt="PRO Barber" height={24} width={120}/>
-        </Link>
+    <header>
+      <Card className="rounded-t-none">
+        <CardContent className="p-5 flex items-center justify-between">
+          <Link href="/">
+            <Image src="/ProBarber.svg" alt="PRO Barber" height={24} width={120}/>
+          </Link>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon">
-              <MenuIcon size={20}/>
-            </Button>
-          </SheetTrigger>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={20}/>
+              </Button>
+            </SheetTrigger>
 
-          <SheetContent className="p-0">
-            <SideMenu />          
-          </SheetContent>
-        </Sheet>
-      </CardContent>
-    </Card>
+            <SheetContent className="p-0">
+              <SideMenu />          
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 }
